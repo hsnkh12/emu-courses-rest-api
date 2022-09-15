0a23ee539932dfa6aa24e66873c01114d228e5d2
Hassan El Abdallah <hassankh613@gmail.com>
2022-09-15 23:03:27 +0300

    add courses serializers and course countroller

--- apps/courses/controllers.py
+++ apps/courses/controllers.py
@@ -2,28 +2,45 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-
-
+from rest_framework.filters import SearchFilter
+from .serializers import CourseSerializer, ResourceSerializer
+from .models import Course, Resource, Like
 
 
 class CoursesController(ViewSet):
 
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name']
+    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filterset_fields = ['code','name','department__name']
+    search_fields = ['name__startswith','code__startswith']
 
     def list(self, request):
-        pass
+
+       queryset = self.get_queryset()
+       serializer = CourseSerializer(queryset, fields=['code','name'] ,many=True)
+
+       return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        pass
+
+        queryset = self.get_queryset().get(code=pk)
+        serializer = CourseSerializer(queryset)
+
+        return Response(serializer.data)
+
+    def get_queryset(self):
+        return Course.objects.select_related('department')
+
 
 class ResourcesController(ViewSet):
 
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['course__id']
+    filterset_fields = ['course__code','title']
 
     def list(self, request):
         pass
 
     def create(self, request):
+        pass
+
+    def update(self, request, pk=None):
         pass
--- apps/courses/serializers.py
+++ apps/courses/serializers.py
@@ -1,2 +1,34 @@
 from ..utils.serializers import DynamicFieldsModelSerializer
+from .models import Course, Resource, Like
+from rest_framework import serializers
+from django.conf import settings
 
+class CourseSerializer(DynamicFieldsModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class LikeSerializer(serializers.Serializer):
+
+    class Meta:
+        model = Like
+        fields = ['is_liked','user']
+
+
+class UserSerializer(serializers.Serializer):
+
+    class Meta:
+        model = settings.AUTH_USER_MODEL
+        fields = ['username']
+
+
+class ResourceSerializer(DynamicFieldsModelSerializer):
+
+    user = UserSerializer()
+    likes = LikeSerializer(many=True)
+
+    class Meta:
+        model = Resource
+        fields = ['id','user','title','url','description','date_added','likes']
+        read_only_fields = ['id','user','likes']
